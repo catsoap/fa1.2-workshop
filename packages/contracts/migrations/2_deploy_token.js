5ac0f69f7b438a04dfeb0d9f06d958d61fa4a0a5
catsoap <sjehann@gmail.com>
2020-12-08 01:08:33 +0100

    add token deploy script

---
+++ packages/contracts/migrations/2_deploy_token.js
@@ -0,0 +1,19 @@
+const { MichelsonMap } = require("@taquito/taquito");
+const Token = artifacts.require("TokenFA12");
+const saveContractAddress = require('../helpers/saveContractAddress');
+
+module.exports = async(deployer, _network, accounts) => {
+    const totalSupply = "1000";
+    const storage = {
+        totalSupply,
+        ledger: MichelsonMap.fromLiteral({
+            [accounts[0]]: {
+                balance: totalSupply,
+                allowances: MichelsonMap.fromLiteral({}),
+            },
+        }),
+    };
+
+  await deployer.deploy(Token,storage)
+        .then(({address}) => saveContractAddress('token', address));
+};
