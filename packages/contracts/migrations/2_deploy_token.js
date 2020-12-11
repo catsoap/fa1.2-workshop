f2109ff466d0018cee1eb54988c8ba678383eb02
catsoap <sjehann@gmail.com>
2020-12-11 22:00:45 +0100

    fix mint

--- packages/contracts/migrations/2_deploy_token.js
+++ packages/contracts/migrations/2_deploy_token.js
@@ -1,19 +1,21 @@
-const { MichelsonMap } = require("@taquito/taquito");
-const Token = artifacts.require("TokenFA12");
+/* global artifacts */
+const { MichelsonMap } = require('@taquito/taquito');
+
+const Token = artifacts.require('TokenFA12');
 const saveContractAddress = require('../helpers/saveContractAddress');
 
-module.exports = async(deployer, _network, accounts) => {
-    const totalSupply = "1000";
-    const storage = {
-        totalSupply,
-        ledger: MichelsonMap.fromLiteral({
-            [accounts[0]]: {
-                balance: totalSupply,
-                allowances: MichelsonMap.fromLiteral({}),
-            },
-        }),
-    };
+module.exports = async (deployer, _network, accounts) => {
+  const totalSupply = '1000000000';
+  const storage = {
+    totalSupply,
+    ledger: MichelsonMap.fromLiteral({
+      [accounts[0]]: {
+        balance: totalSupply,
+        allowances: MichelsonMap.fromLiteral({}),
+      },
+    }),
+  };
 
-  await deployer.deploy(Token,storage)
-        .then(({address}) => saveContractAddress('token', address));
+  await deployer.deploy(Token, storage)
+    .then(({ address }) => saveContractAddress('token', address));
 };
--- packages/contracts/test/token.test.js
+++ packages/contracts/test/token.test.js
@@ -1,3 +1,4 @@
+/* global artifacts contract it tezos  */
 const TokenFA12 = artifacts.require('TokenFA12');
 const assert = require('assert');
 const { InMemorySigner } = require('@taquito/signer');
@@ -10,21 +11,21 @@ contract('TokenFA12', async () => {
     const aliceAddress = accounts.alice.pkh;
     const { totalSupply } = storage;
     const aliceRecord = await storage.ledger.get(aliceAddress);
-    assert.strictEqual(totalSupply.toNumber(), 1000);
-    assert.strictEqual(aliceRecord.balance.toNumber(), 1000);
+    assert.strictEqual(totalSupply.toNumber(), 1000000000);
+    assert.strictEqual(aliceRecord.balance.toNumber(), 1000000000);
   });
 
   it('should transfer tokens from Alice to Bob', async () => {
     const instance = await TokenFA12.deployed();
     const aliceAddress = accounts.alice.pkh;
     const bobAddress = accounts.bob.pkh;
-    const value = 100;
+    const value = 100000000;
     await instance.transfer(aliceAddress, bobAddress, value);
     const storage = await instance.storage();
     const aliceRecord = await storage.ledger.get(aliceAddress);
     const bobRecord = await storage.ledger.get(bobAddress);
-    assert.strictEqual(aliceRecord.balance.toNumber(), 900);
-    assert.strictEqual(bobRecord.balance.toNumber(), 100);
+    assert.strictEqual(aliceRecord.balance.toNumber(), 900000000);
+    assert.strictEqual(bobRecord.balance.toNumber(), 100000000);
   });
 
   it("should fail if transfer isn't approved", async () => {
@@ -34,7 +35,7 @@ contract('TokenFA12', async () => {
       await InMemorySigner.fromSecretKey(accounts.bob.sk),
     );
     const bobAddress = accounts.bob.pkh;
-    const value = 10000;
+    const value = 1;
     await assert.rejects(
       instance.transfer(aliceAddress, bobAddress, value),
       (err) => {
@@ -45,15 +46,15 @@ contract('TokenFA12', async () => {
     );
   });
 
-  it('should mint 1 token withe default entry point', async () => {
+  it('should mint 1 token with 1tez', async () => {
     const instance = await TokenFA12.deployed();
     const bobAddress = accounts.bob.pkh;
     tezos.setSignerProvider(
       await InMemorySigner.fromSecretKey(accounts.bob.sk),
     );
-    await tezos.contract.transfer({ to: instance.address, amount: 1 });
+    await instance.mint(null, { amount: '0.001' });
     const storage = await instance.storage();
     const bobRecord = await storage.ledger.get(bobAddress);
-    assert.strictEqual(bobRecord.balance.toNumber(), 101);
+    assert.strictEqual(bobRecord.balance.toNumber(), 100001000);
   });
 });
