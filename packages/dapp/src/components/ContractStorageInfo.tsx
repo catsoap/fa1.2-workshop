ec99a3a0313414b4dc43b0d042a64408f384e23c
catsoap <sjehann@gmail.com>
2020-12-24 11:56:52 +0100

    dapp get storage

---
+++ packages/dapp/src/components/ContractStorageInfo.tsx
@@ -0,0 +1,22 @@
+import { useCallback } from 'react';
+import useBeacon from '../hooks/useBeacon';
+import { usePendingPromise } from '../hooks/usePendingPromise';
+
+export const ContractStorageInfo: React.FC<{ contractAddress: string }> = ({ contractAddress }) => {
+    const { Tezos } = useBeacon();
+    const fetcher = useCallback(async () => (await Tezos.contract.at(contractAddress)).storage(), [
+        Tezos.contract,
+        contractAddress,
+    ]);
+
+    const { fetching, data, error } = usePendingPromise(fetcher, JSON.stringify);
+    console.log({ fetching, data, error });
+
+    return !fetching && (error || data) ? (
+        <div>
+            Contract Storage:<pre>{error || data}</pre>
+        </div>
+    ) : (
+        <span>loading...</span>
+    );
+};
--- packages/dapp/src/config.ts
+++ packages/dapp/src/config.ts
@@ -1,1 +1,4 @@
-export const RPC: string = process.env.REACT_APP_RPC ?? '';
+import token from '@fa1.2-workshop/contracts/deployments/token';
+
+export const RPC: string = process.env.REACT_APP_RPC ?? 'http://localhost:8732';
+export const CONTRACT_ADDRESS = token;
