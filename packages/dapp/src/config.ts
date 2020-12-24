8d04ae9fe15803f2fe4b747b5983fcb166862473
catsoap <sjehann@gmail.com>
2020-12-24 12:09:29 +0100

    fix env

--- packages/dapp/src/config.ts
+++ packages/dapp/src/config.ts
@@ -1,4 +1,4 @@
 import token from '@fa1.2-workshop/contracts/deployments/token';
 
-export const RPC: string = process.env.REACT_APP_RPC ?? 'http://localhost:8732';
+export const RPC: string = process.env.REACT_APP_RPC ?? '';
 export const CONTRACT_ADDRESS = token;
